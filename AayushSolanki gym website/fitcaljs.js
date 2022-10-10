0b878973223ce693c14f93f2d6ad1fe4a7daee23
Nguyễn Duy Cương (Harry Tran) <harrytran103@gmail.com>
2022-10-10 17:22:28 +0700

    Revert "Gym Website"

--- AayushSolanki gym website/fitcaljs.js
+++
@@ -1,5 +0,0 @@
- function calcBMI() {
-  var weight = document.bmiform.pounds.value, height = document.bmiform.inches.value;
-  document.bmiform.bmi.value = parseInt((weight * 703) / (height * height));
-} 
-
